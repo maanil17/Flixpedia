c2f3475cc37ef67619de57d0b1fb3d311f776349
maanil17 <mlaad17@gmail.com>
2023-06-08 01:46:13 +0530

    testing123

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -56,7 +56,7 @@ const HeroBanner = () => {
 
         <div className="heroBannerContent">
           <span className="title">
-Flixpesddiaa</span>
+Flixpesdddddiaa</span>
           <span className="subTitle">
 Discover endless movies, TV shows, and captivating people. Start exploring now.</span>
 
